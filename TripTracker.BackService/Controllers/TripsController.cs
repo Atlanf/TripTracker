fcc021e3396f18495962d29eddf5771a402d4ed7
Roman Yarmolovich <Smile182382@gmail.com>
2020-02-02 14:58:31 +0300

    First Module

---
+++ TripTracker.BackService/Controllers/TripsController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TripTracker.BackService.Model;
+
+namespace TripTracker.BackService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TripsController : ControllerBase
+    {
+        private Repository _repository;
+
+        public TripsController(Repository repository)
+        {
+            _repository = repository;
+        }
+
+        // GET api/trips
+        [HttpGet]
+        public ActionResult<IEnumerable<Trip>> Get()
+        {
+            return _repository.Get();
+        }
+
+        // GET api/trips/5
+        [HttpGet("{id}")]
+        public ActionResult<Trip> Get(int id)
+        {
+            return _repository.Get(id);
+        }
+
+        // POST api/trips
+        [HttpPost]
+        public void Post([FromBody] Trip value)
+        {
+            _repository.Add(value);
+        }
+
+        // PUT api/trips/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Trip value)
+        {
+            _repository.Update(value);
+        }
+
+        // DELETE api/trips/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            _repository.Remove(id);
+        }
+    }
+}
